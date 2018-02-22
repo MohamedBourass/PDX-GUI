84e5d177b8d59f3978b90eb76d60a54de970b5d7
momal <bourass@gmail.com>
2018-02-22 23:26:08 +0100

    Add statistics

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -234,4 +234,31 @@ public class MainApp extends Application {
 
 	  }
 	}
+	
+    /**
+     * Opens a dialog to show birthday statistics. 
+     */
+    public void showBirthdayStatistics() {
+      try {
+        // Load the fxml file and create a new stage for the popup
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/BirthdayStatistics.fxml"));
+        AnchorPane page = (AnchorPane) loader.load();
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("Birthday Statistics");
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        dialogStage.initOwner(primaryStage);
+        Scene scene = new Scene(page);
+        dialogStage.setScene(scene);
+
+        // Set the persons into the controller
+        BirthdayStatisticsController controller = loader.getController();
+        controller.setPersonData(personData);
+
+        dialogStage.show();
+
+      } catch (IOException e) {
+        // Exception gets thrown if the fxml file could not be loaded
+        e.printStackTrace();
+      }
+    }
 }
